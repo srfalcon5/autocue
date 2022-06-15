0dca72886e1af3f24c7e43671403ba34f759e0f4
Matthew Ronchetto <hello@doamatto.xyz>
2022-06-15 08:15:01 -0700

    chore: add blank refs

---
+++ web/app.js
@@ -0,0 +1,10 @@
+function handleMarquee() {
+	const marquee = document.getElementById("marquee");
+	let speed = 4 | customSpeed;
+	let lastScrollPos = 0;
+	let timer;
+
+	const container = marquee.querySelector(".inner");
+	const content = marquee.querySelector(".inner > p");
+}
+
