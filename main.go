6f6d0b4bdf8b7675b633e261243447cd8af9593a
Matthew Ronchetto <hello@doamatto.xyz>
2022-07-09 09:20:22 -0700

    fix: typos and missing deps

--- main.go
+++ main.go
@@ -1,33 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
 )
 
 func main() {
 	// Serve web files
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile("web/prompter.html")
+		http.ServeFile(w, r, "web/prompter.html")
 	})
 	http.HandleFunc("/privacy", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile("web/privacy.html")
+		http.ServeFile(w, r, "web/privacy.html")
 	})
 	http.HandleFunc("/app.css", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile("web/app.css")
+		http.ServeFile(w, r, "web/app.css")
 	})
 	http.HandleFunc("/app.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile("web/app.js")
+		http.ServeFile(w, r, "web/app.js")
 	})
 	http.HandleFunc("/scripts", func(w http.ResponseWriter, r *http.Request) {
-		http.FileServer(http.Dir("scripts/")
+		http.FileServer(http.Dir("scripts/"))
 	})
 	http.HandleFunc("/robots.txt", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile("scripts/robots.txt") // Basic privacy for scripts
+		http.ServeFile(w, r, "scripts/robots.txt") // Basic privacy for scripts
 	})
 
 	// API endpoints
 	http.Handle("/storescript", scriptStore)
-	http.Handle()
 
 	log.Fatal(http.ListenAndServe(":2023", nil))
 }
