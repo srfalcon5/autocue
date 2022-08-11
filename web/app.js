53e119a58731a345a4b746fc880b89cf29f933dd
Matthew Ronchetto <hello@doamatto.xyz>
2022-08-10 22:19:35 -0700

    chore: remove debugging and shrink some code

--- web/app.js
+++ web/app.js
@@ -30,7 +30,6 @@ function marquee() {
 		const obj = document.getElementById("marquee");
 		curScroll = obj.scrollTop;
 		if (curScroll + obj.clientHeight == document.clientHeight) {
-			console.log("0 0");
 			obj.scrollTo(0, 0);
 		} else {
 			obj.scrollTo({
@@ -44,18 +43,13 @@ function marquee() {
 // Handle scrolling start and stop
 document.getElementById("toggle").addEventListener("click", () => {
 	play = !play;
-	if (play) {
-		console.log("scrolling");
-	} else {
-		console.log("not scrolling");
-	}
 	return false;
 }, false);
 
 // Handle speed changes
 document.getElementById("speed").addEventListener("change", () => {
-	const index = document.getElementById("speed").selectedIndex;
-	speed = speeds[index];
+	const i = document.getElementById("speed").selectedIndex;
+	speed = speeds[i];
 	console.log("destroy scroller")
 	clearInterval(scroller);
 	scroller = setInterval(marquee, speed);
@@ -65,25 +59,25 @@ document.getElementById("speed").addEventListener("change", () => {
 
 // Handle text size changes
 document.getElementById("size").addEventListener("change", () => {
-	const index = document.getElementById("size").selectedIndex;
-	document.getElementById('marquee').style.fontSize = fontSizes[index];
+	const i = document.getElementById("size").selectedIndex;
+	document.getElementById('marquee').style.fontSize = fontSizes[i];
 	return false;
 }, false);
 
 // Handle alignment changes
 document.getElementById("align").addEventListener("change", () => {
-	const index = document.getElementById("align").selectedIndex;
-	document.getElementById("marquee").style.textAlign = align[index];
+	const i = document.getElementById("align").selectedIndex;
+	document.getElementById("marquee").style.textAlign = align[i];
 	return false;
 }, false);
 
 // Handle mirror changes
 document.getElementById("mirror").addEventListener("change", () => {
-	const index = document.getElementById("mirror").selectedIndex;
+	const i = document.getElementById("mirror").selectedIndex;
 	const obj = document.getElementById("marquee"); 
-	if (index == 0) {
+	if (i == 0) {
 		obj.style.transform = "scale(1,1)";
-	} else if (index == 1) {
+	} else if (i == 1) {
 		obj.style.transform = "scale(-1,1)";
 	}
 	return false;
